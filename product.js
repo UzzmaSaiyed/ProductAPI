9c7465d0124202ab4729cb4be60d9b324ce2b4b3
Uzzmasaiyed <writeruzzi@gmail.com>
2024-11-06 13:56:36 +0530

    update

--- product.js
+++ product.js
@@ -52,3 +52,14 @@ let product = [
 ];
 
 module.exports = product;
+
+function getProducts() {
+    fetch('http://localhost:3000/api/product')  // Adjust this to your hosted API URL
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('output').textContent = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            document.getElementById('output').textContent = 'Error fetching products: ' + error;
+        });
+}
